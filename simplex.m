9dca1597a8929d6e9deb55001417e4c4d7dde49e
Shubham Gandhi <100235114+9550shubham@users.noreply.github.com>
2023-04-24 02:14:38 +0530

    Update simplex.m

--- simplex.m
+++ simplex.m
@@ -5,25 +5,25 @@ cost=[2 1 0 0 0 0];
  b=[10;6;2];
 A=[a b];
  Var={'x1','x2','s1','s2','s3','sol'}
- bv=[3 4 5]; % position of s1, s2, s3
+ bv=[3 4 5]; 
 zjcj=cost(bv)*A-cost;   
 
-% Display initial simplex table
+
 simplex_table=[A; zjcj]
 array2table(simplex_table,'VariableNames',Var)
 RUN=true;
  while RUN
-if any(zjcj(1:end-1)<0) % check for negative value optamility condition
+if any(zjcj(1:end-1)<0) 
  fprintf(' The current BFS is not optimal \n');
  zc=zjcj(1:end-1);
- [Enter_val, pvt_col]= min(zc) ; %array 
+ [Enter_val, pvt_col]= min(zc) ;
  if all(A(:,pvt_col)<=0)
   error('LPP is Unbounded');
  else
  sol=A(:,end);
  column=A(:,pvt_col);
   for i=1:size(A,1)
- if column(i)>0   % pivot column value positive
+ if column(i)>0   
  ratio(i)= sol (i)./column(i);
  else
  ratio(i)=inf;
@@ -31,10 +31,10 @@ if any(zjcj(1:end-1)<0) % check for negative value optamility condition
   end 
   [leaving_value,pvt_row]=min(ratio);
  end
- bv(pvt_row)=pvt_col;    % replaced leaving variable with entering variable
+ bv(pvt_row)=pvt_col;   
  pvt_key=A(pvt_row, pvt_col);
  A(pvt_row,:)=A (pvt_row,:)./pvt_key;
- % row operation 
+ 
 for i=1:size(A,1)
  if i~=pvt_row
  A(i,:)=A(i,:)-A (i, pvt_col).*A(pvt_row,:);
